b823dd2caef0f4f560253b27f645ebf39c0708c3
lishi <lishi@blackshine.cn>
2024-09-19 19:11:26 +0800

    init

---
+++ src/views/pms/user/api.js
@@ -0,0 +1,19 @@
+/**********************************
+ * @Author: Ronnie Zhang
+ * @LastEditor: Ronnie Zhang
+ * @LastEditTime: 2023/12/05 21:29:51
+ * @Email: zclzone@outlook.com
+ * Copyright © 2023 Ronnie Zhang(大脸怪) | https://isme.top
+ **********************************/
+
+import { request } from '@/utils'
+
+export default {
+  create: data => request.post('/admin/user/add', data),
+  read: (data = {}) => request.post('/admin/user/base_list', data),
+  update: data => request.put(`/admin/user/update`, data),
+  delete: ids => request.delete(`/admin/user/delete`, { data: { ids } }),
+  relation_role: data => request.post(`/admin/user/relation_role`, data),
+  resetPwd: data => request.post(`/admin/user/reset_pwd`, data),
+  getAllRoles: () => request.post('/admin/role/list', { filters: { enable: 1 } }),
+}
